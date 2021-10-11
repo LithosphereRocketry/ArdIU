4f133ac058c0c7790ec5b864804cb5382e3c8d90
LithosphereRocketry <33179745+LithosphereRocketry@users.noreply.github.com>
2021-10-11 00:01:24 -0400

    add comments to ArdIU.h

--- ArdIU.h
+++ ArdIU.h
@@ -177,78 +177,99 @@ public:
 	
 	static volatile bool imuInterrupt; // IMU interrupt status flag
 	
-	static long int tBurnout;
-	static long int tApogee;
-	static long int tLiftoff;
+	static long int tBurnout; // time, relative to millis() system clock, of burnout detection
+	static long int tApogee; // time, relative to millis() system clock, of apogee detection
+	static long int tLiftoff; // time, relative to millis() system clock, of liftoff detection
 	
-	static float altApogee;
-	static float altitude;
+	static float altApogee; // highest altitude recorded so far
+	static float altitude; // current altitude as of last data frame
 	
-	static void getIMU();
+	static void getIMU(); // pull data from IMU buffer
 	
-	static float getAccelX() { return accel.x/1024.0; }
+	// get acceleration values, converted from unitless sensor values
+	static float getAccelX() { return accel.x/1024.0; } 
 	static float getAccelY() { return accel.y/1024.0; }
 	static float getAccelZ() { return accel.z/1024.0; }
 	static float getAccel()  { return accel.getMagnitude()/1024.0; }
-	static VectorFloat getVF(VectorInt16 vector) { return VectorFloat(vector.x, vector.y, vector.z); }
 	
-	static void beepBoolean(bool input, int onTime, int offTime);
+	static VectorFloat getVF(VectorInt16 vector) { return VectorFloat(vector.x, vector.y, vector.z); } // convert a vector from ints to floats
 	
-	class Flag {
+	static void beepBoolean(bool input, int onTime, int offTime); // play a status tone, either true (high note) or false (low note)
+	
+	class Flag { // Flag class allows arbitrary events to be scheduled for a given time; it uses a bitmask to save memory 
+		     // (since booleans are stored as larger types, it actually saves a significant amount)
+		     // the compiler requires this to be in the header for some reason
 	  private:
 		unsigned long int time;  // high bit used for "live" boolean
-		void (*event)();
+		void (*event)(); // low level C hackery to pass a function as a parameter
 	  public:
-		long int getTime() { return time & TIME_MASK; }
-		Flag() {
+		long int getTime() { return time & TIME_MASK; } // mask away the live bit
+		Flag() { // initialize a blank flag
 			time = 0;
 			event = NULL;
 		}
 		void set(long int timeIn, void (*eventIn)()) {
-			time = timeIn & TIME_MASK;
+			time = timeIn & TIME_MASK; // overlay the flag time using the bitmask
 			event = eventIn;
 			setLive();
 		}
-		void getEvent() {
-			if(isLive() && millis() > getTime()) {
-				(*event)();
-				setNotLive();
+		void getEvent() { // see if the event should happen
+			if(isLive() && millis() > getTime()) { // if the time has passed and it's active...
+				(*event)(); // run it
+				setNotLive(); // set it inactive
 			}
 		}
-		void setNotLive() { time = time & TIME_MASK; }
-		void setLive() { time = time | LIVE_FLAG; }
-		bool isLive() { return ((time & LIVE_FLAG) != 0); }
+		
+		void setNotLive() { time = time & TIME_MASK; } // removes active flag from time:
+		//   time      Fxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+		// & TIME_MASK 01111111 11111111 11111111 11111111
+		// = time      0xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+		
+		void setLive() { time = time | LIVE_FLAG; } // add active flag to time:
+		//   time      Fxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+		// | LIVE_FLAG 10000000 00000000 00000000 00000000
+		// = time      1xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+		
+		bool isLive() { return ((time & LIVE_FLAG) != 0); } // reads active flag from time:
+		//   time      Fxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+		// & LIVE_FLAG 10000000 00000000 00000000 00000000
+		// =           F0000000 00000000 00000000 00000000
 	};
-	static byte setFlag(long int met, void (*event)());	
-	static Flag flagBuffer[N_FLAGS];
+	static byte setFlag(long int met, void (*event)()); // add a flag to the buffer
+	static Flag flagBuffer[N_FLAGS]; // buffer of flags that will be automatically handled
 	
-	static MPU6050 imu;
-	static BMP280_DEV* pBaro;
-	static SdFat SD;
-	static bool isIMU, isBaro, isSD;
-	static byte pyroPins[CHANNELS], contPins[CHANNELS];
-	static float groundAlt;
-	// static void setPyroPins(int pyro_pins[CHANNELS], int cont_pins[CHANNELS]);
-	static BetterVectorFloat vertical;
+	static MPU6050 imu; // IMU object
+	static BMP280_DEV* pBaro; // pointer to barometer object because this library prefers that
+	static SdFat SD; // SD card object
+	static bool isIMU, isBaro, isSD; // flags for whether each core system is active
+	static byte pyroPins[CHANNELS], contPins[CHANNELS]; // list of pin assigments for pyrotechnic channels
+	static float groundAlt; // ground altitude
+	static BetterVectorFloat vertical; // vector representing initial acceleration
+	
 	static void setVinDiv(long int res1, long int res2); // set the standard voltage divider for unregulated pins
 	static void setGroundAlt(); // calibrates the ground altitude; preferred against eventually
-	static unsigned long int getMET();
-	static float getAltSmoothed(int e_life); // returns the current pBarometric altitude, with smoothing
-	static float getAlt();
+	
+	static unsigned long int getMET(); // returns time since liftoff detect
+	static float getAlt(); // returns unsmoothed barometric altitude
+	static float getAltSmoothed(int e_life); // returns the current barometric altitude, with smoothing
 	static float getVin(); // returns the voltage at the battery input, or about 0.5V less than VCC when powered over USB
 	static float getVoltage(int analog_in); // scales an analog input to a divided voltage using the standard voltage divider
-	static bool getCont(int channel);
-	static void initSD();
-	static void initBaro();
-	static void initIMU();
-	static void storeBytes(const char* bytes, int size);
-	static float getTilt();
+	static bool getCont(int channel); // returns whether continuity is detected on a given pyro channel
+	static float getTilt(); // returns current off-axis tilt from IMU
+	
+	static void initSD(); // initialize the SD card
+	static void initBaro(); // initialize the barometer
+	static void initIMU(); // initialize the IMU
+	
+	static void storeBytes(const char* bytes, int size); // write bytes to the SD card, only opening the card when the buffer fills
+	
+	// template for writing any data type to the SD card
 	template <class TYPE> 
 	static void store(TYPE data) { // for some reason Arduino only allows templates in headers
 		if(isSD) {
-			const char* bytes = (const char*) &data;
+			const char* bytes = (const char*) &data; // tell C that, whatever this data was, treat it as if it were raw bytes/chars
 		//	flightLog.write(bytes, sizeof(TYPE));
-			storeBytes(bytes, sizeof(TYPE));
+			storeBytes(bytes, sizeof(TYPE)); // write to the card
 		}
 	}
 	
@@ -256,26 +277,26 @@ public:
 #define MAX_FILE_LEN 16
 
 private:
-	static void dmpDataReady();
+	static void dmpDataReady(); // returns whether data is waiting
 //	static File flightLog;
-	static char filename[MAX_FILE_LEN];
-	static byte buffer[BUF_SIZE];
-	static long int SDPos;
-	static long int lastBaro;
-	static byte imuFifoBuffer[IMU_BUFFER_SIZE];
-	static float smooth1;
+	static char filename[MAX_FILE_LEN]; // current filename to write to
+	static byte buffer[BUF_SIZE]; // current data buffer to be written
+	static long int SDPos; // current SD card write location
+	static long int lastBaro; // last time a barometer reading was recorded, in MET
+	static byte imuFifoBuffer[IMU_BUFFER_SIZE]; // buffer for incoming IMU data
+	static float smooth1; // smoothing parameters for barometer data
 	static float smooth2;
 	static float smooth3;
-	static float vinScale;
-	static Quaternion imuQ;
-	static VectorInt16Improved accel;
-//	static VectorInt16Improved accelWorld; // Gravity doesn't necessarily work with +/-16G mode
+	static float vinScale; // scaling factor for battery readings
+	static Quaternion imuQ; // rotation from IMU
+	static VectorInt16Improved accel; // acceleration from IMU
+//	static VectorInt16Improved accelWorld; // Gravity doesn't necessarily work with +/-16G mode so these are ignored
 //	static VectorFloat gravity;
 	
-	static unsigned int imuPacketSize;
-	static byte imuDevStatus;
-	static byte bytesBuffered;
-	static byte apogeeFlag, burnoutFlag, liftoffFlag;
-    static void restartFlag(byte &flag, void (*event)(), int time);
+	static unsigned int imuPacketSize; // packet size for IMU buffer
+	static byte imuDevStatus; // status reading from IMU
+	static byte bytesBuffered; // number of bytes currently waiting to be written to card
+	static byte apogeeFlag, burnoutFlag, liftoffFlag; // addresses of various important flags; this might be reworked later
+	static void restartFlag(byte &flag, void (*event)(), int time); // restart a flag's timer
 };
 #endif
