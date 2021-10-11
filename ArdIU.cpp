b3c79c6e6e9c4a26ccdccf859f5d5d3827d76245
LithosphereRocketry <33179745+LithosphereRocketry@users.noreply.github.com>
2021-10-11 01:13:58 -0400

    add comments to ArdIU.cpp

--- ArdIU.cpp
+++ ArdIU.cpp
@@ -54,11 +54,12 @@ volatile bool ArdIU::imuInterrupt;
 void ArdIU::dmpDataReady() { imuInterrupt = true; }
  
 void ArdIU::beepBoolean(bool input, int onTime, int offTime) {
-	tone(BUZZER, 1000+(input?1000:0), onTime);
-	delay(onTime+offTime);
+	tone(BUZZER, 1000+(input?1000:0), onTime); // tone is parallel because it runs in a system register or something...
+	delay(onTime+offTime); // so include on time in delay
 }
 
 void ArdIU::begin() {
+	// initialize pyro pinout
 	for(int i = 0; i < CHANNELS; i++) { // first 3 channels default, all others NO_CHANNEL
 		if(i < 3) {
 			pyroPins[i] = i+3;  // 3, 4, 5
@@ -72,15 +73,14 @@ void ArdIU::begin() {
 	pinMode(LED, OUTPUT);
 	pinMode(CS_SD, OUTPUT);
 	digitalWrite(CS_SD, HIGH);
-
 	pinMode(VIN, INPUT);
 	
 	initIMU();
-	beepBoolean(isIMU, 100, 50);
+	beepBoolean(isIMU, 100, 50); // check IMU, status tone
 	initSD();
 	beepBoolean(isSD, 100, 50); // check SD, status tone
 	initBaro();
-	beepBoolean(isBaro, 100, 50); // check pBaro, status tone
+	beepBoolean(isBaro, 100, 50); // check baro, status tone
 	
 	
 	for(int i = 0; i < CHANNELS; i++) { // setup pyroPins, beep out continuity
@@ -94,12 +94,6 @@ void ArdIU::begin() {
 	
 	setGroundAlt(); // establish zero
 }
-/* void ArdIU::setPyroPins(int pyro_pins[CHANNELS], int cont_pins[CHANNELS]) { // set all the output pins
-	for(int i = 0; i < CHANNELS; i++) {
-		pyroPins[i] = pyro_pins[i];
-		contPins[i] = cont_pins[i];
-	}
-} */
 void ArdIU::setVinDiv(long int resGnd, long int resVin) { // for other devices, set the VIN voltage divider
 	vinScale = ((float) resGnd)/(resGnd+resVin);
 }
@@ -108,7 +102,7 @@ void ArdIU::setGroundAlt() {
 		getAlt();  // sometimes first reading is bogus so dump it
 		float total = 0.0;
 		const int num = 20;
-		for(int i = 0; i < num; i++) {
+		for(int i = 0; i < num; i++) { // take a bunch of readings, 100ms apart, to correct for wind, etc
                     delay(100);
                     float alt = getAlt();
                     // Serial.println(alt);
@@ -120,27 +114,27 @@ void ArdIU::setGroundAlt() {
 unsigned long int ArdIU::getMET() {
 	if(tLiftoff > 0) {
 		return millis()-tLiftoff;
-	} else { return 0; }
+	} else { return 0; } // if we haven't lifted off, leave MET at 0
 }
 
 float ArdIU::getAlt() {
-  float alt = 0.0;
-  pBaro -> startForcedConversion();
-  while(! pBaro -> getAltitude(alt));
+  float alt = 0.0; // give the altitude somewhere to go
+  pBaro -> startForcedConversion(); // tell the barometer to start reading
+  while(! pBaro -> getAltitude(alt)); // wait until we get a reading, then dump it in alt; this is by reference and therefore looks kinda weird
   return alt;
 }
 
 float ArdIU::getAltSmoothed(int e_life) {
 	// Quadratic exponential smoothing algorithm, a less known algorithm that attempts to fit data to a parabola
 	// https://en.wikipedia.org/wiki/Exponential_smoothing
-	float beta = exp(-((float) (getMET()-lastBaro))/e_life);
+	float beta = exp(-((float) (getMET()-lastBaro))/e_life); // beta is basically an exponential decay parameter
 	float alpha = 1.0 - beta;
 	smooth1 = (getAlt()-groundAlt)*alpha + beta*smooth1;
 	smooth2 = smooth1*alpha + beta*smooth2;
 	smooth3 = smooth2*alpha + beta*smooth3;
-   
+   	
 	lastBaro = getMET();
-	return 3*(smooth1 - smooth2) + smooth3;
+	return 3*(smooth1 - smooth2) + smooth3; // gonna be honest, I'm not sure how this works
 	// float b = (alpha / (2*sq(1-alpha)) * ((6 - 5*alpha)*smooth1 - (10 - 8*alpha)*smooth2 + (4 + 3*alpha)*smooth3);
 	// float c = (sq(alpha) / sq(1-alpha)) * (smooth1 - 2*smooth2 + smooth3);
 	
@@ -149,7 +143,7 @@ float ArdIU::getVoltage(int analog_in) {
 	return analog_in*3.3/vinScale/1024.0; // reading -> voltage @ pin -> voltage @ input
 }
 float ArdIU::getVin() {
-	return getVoltage(analogRead(VIN));
+	return getVoltage(analogRead(VIN)); // packaging these two functions for convenience
 }
 boolean ArdIU::getCont(int channel) {
 	float voltage = getVoltage(analogRead(contPins[channel]));
@@ -158,49 +152,49 @@ boolean ArdIU::getCont(int channel) {
 
 #define MAX_FILES 100
 void ArdIU::initSD() {
-	isSD = SD.begin(CS_SD);
-        filename[0] = 0;  // strcpy(filename, "")
-	if(isSD) {
+	isSD = SD.begin(CS_SD); // initialize SD library
+        filename[0] = 0;  // strcpy(filename, "") by exploiting properties of null-terminated strings
+	if(isSD) { // if the card exists
 		int i = 0;
-		while(i < MAX_FILES) {
+		while(i < MAX_FILES) { // iterate until we find a file that still hasn't been written
 			char tryfilename[MAX_FILE_LEN];
 			char num[4];
-			itoa(i, num, 10);
+			itoa(i, num, 10); // convert the file number to a C string/char array
 			strcpy(tryfilename, "flight");
 			strcat(tryfilename, num);
-			strcat(tryfilename, ".aiu");
-			if (!SD.exists(tryfilename)) {
-				strcpy(filename,tryfilename);
-				break;
+			strcat(tryfilename, ".aiu"); // punch in flightXX.aiu via C string operations
+			if (!SD.exists(tryfilename)) { // if the file is available...
+				strcpy(filename,tryfilename); // make it official
+				break; // we're done here
 			}
 			i++;
 		} 
-		SDPos = 0;
+		SDPos = 0; // start at beginning of file
 		store((long int) 3); // version header
 	}
 }
 
 void ArdIU::storeBytes(const char* bytes, int size) { // to prevent inlining
-	for(int i = 0; i < size; i++) {
-		buffer[bytesBuffered] = bytes[i];
+	for(int i = 0; i < size; i++) { // iterate over data given
+		buffer[bytesBuffered] = bytes[i]; // add each byte to the buffer & increment count
 		bytesBuffered++;
-		if(bytesBuffered >= BUF_SIZE) {
-			File flightLog = SD.open(filename, FILE_WRITE);
-			flightLog.seek(SDPos);
-			int written = flightLog.write(buffer, BUF_SIZE);
-			SDPos = flightLog.position();
-			flightLog.close();
-			bytesBuffered = 0;
+		if(bytesBuffered >= BUF_SIZE) { // if it's full...
+			File flightLog = SD.open(filename, FILE_WRITE); // open the SD card
+			flightLog.seek(SDPos); // go to the proper position
+			int written = flightLog.write(buffer, BUF_SIZE); // dump data
+			SDPos = flightLog.position(); // read our new position
+			flightLog.close(); // save file
+			bytesBuffered = 0; // reset buffer
 		}
 	}
 }
 
 void ArdIU::initBaro() {
-	if(pBaro) { delete pBaro; }
-	if(SCK == 13 && MISO == 12 && MOSI == 11) {
+	if(pBaro) { delete pBaro; } // if it already exists, reset it
+	if(SCK == 13 && MISO == 12 && MOSI == 11) { // if we're on default pinout, set up barometer with hardware SPI
 		pBaro = new BMP280_DEV(CS_BARO);
-		isBaro = pBaro -> begin(SLEEP_MODE, OVERSAMPLING_X4, OVERSAMPLING_X1, IIR_FILTER_OFF, TIME_STANDBY_05MS);  
-	} else {
+		isBaro = pBaro -> begin(SLEEP_MODE, OVERSAMPLING_X4, OVERSAMPLING_X1, IIR_FILTER_OFF, TIME_STANDBY_05MS);
+	} else { // assume there's no barometer
 		pBaro = NULL;
 		isBaro = false;
 	}
@@ -208,6 +202,7 @@ void ArdIU::initBaro() {
 
 void ArdIU::initIMU() {
 	// Based on code by Jeff Rowberg
+	// initialize available I2C implementation
 	#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 		Wire.begin();
 		#ifndef WIRE_COMPAT_MODE
@@ -217,38 +212,38 @@ void ArdIU::initIMU() {
 		Fastwire::setup(400, true);
 	#endif
 	
-	imu.initialize();
+	imu.initialize(); // start IMU & ready interrupt
 	pinMode(INTERRUPT, INPUT);
 	
-	isIMU = imu.testConnection();
+	isIMU = imu.testConnection(); // test if it worked
 
 	imuDevStatus = imu.dmpInitialize();
 
         struct calibrationData { int ax, ay, az, gx, gy, gz; } calData;
-	EEPROM.get(0, calData);
+	EEPROM.get(0, calData); // read calibration values from EEPROM
 	
-	imu.setXGyroOffset(calData.gx);
+	imu.setXGyroOffset(calData.gx); // set calibration data
 	imu.setYGyroOffset(calData.gy);
 	imu.setZGyroOffset(calData.gz);
 	imu.setXAccelOffset(calData.ax);
 	imu.setYAccelOffset(calData.ay);
 	imu.setZAccelOffset(calData.az);
 	
-	if(imuDevStatus == 0 && isIMU) {
-		imu.setDMPEnabled(true);
+	if(imuDevStatus == 0 && isIMU) { // if the IMU exists...
+		imu.setDMPEnabled(true); // enable offboard processing
 		
-		attachInterrupt(digitalPinToInterrupt(INTERRUPT), dmpDataReady, RISING);
+		attachInterrupt(digitalPinToInterrupt(INTERRUPT), dmpDataReady, RISING); // set up interrupt
 		imuInterrupt = false;
 		
-		imuPacketSize = imu.dmpGetFIFOPacketSize();
+		imuPacketSize = imu.dmpGetFIFOPacketSize(); // set packet size
 		
 		imu.setFullScaleAccelRange(MPU6050_ACCEL_FS_16); // set accel to +/-16 G mode
-	} else { isIMU = false; }
+	} else { isIMU = false; } // otherwise ignore it
 }
 void ArdIU::getIMU() {
 	// Based on code by Jeff Rowberg
 
-	while(1) {
+	while(1) { // until we have all of our data...
         	// get current FIFO count
 	        unsigned int fifoCount = imu.getFIFOCount();
 
@@ -273,31 +268,33 @@ void ArdIU::getIMU() {
                         break;
 		}
 	};
-	imuInterrupt = false;
+	imuInterrupt = false; // we've received the interrupt
 	
-	imu.dmpGetQuaternion(&imuQ, imuFifoBuffer);
+	imu.dmpGetQuaternion(&imuQ, imuFifoBuffer); // unpack the data we just got
 	imu.dmpGetAccel(&accel, imuFifoBuffer);
 }
 
 void ArdIU::logData(int baro_e_life) {
-	long int met = getMET();
+	long int met = getMET(); // store current mission clock
 	store(met);
 	long int statemask = 0;
-	statemask += (byte) (getVin()*16); //0-7: battery voltage (0-16V * 16)
+	
+	// STATEMASK THINGS
+	statemask += (byte) (getVin()*16); // bits 0-7: battery voltage (0-16V * 16)
 	for(int i = 0; i < CHANNELS && i < 8; i++) {
-		statemask |= getCont(i) << (i+8);
+		statemask |= getCont(i) << (i+8); // place a 1 at each position that has continuity from bits 8 to 15
 	}
 	statemask += ((long) isIMU << 16) | ((long) isSD << 17) | ((long) isBaro << 18) |
-                     ((long) isLiftoff() << 24) | ((long) isBurnout() << 25) | ((long) isApogee() << 26);
+                     ((long) isLiftoff() << 24) | ((long) isBurnout() << 25) | ((long) isApogee() << 26); // place sensor states and flight states in corresponding bit positions in statemask
 	store(statemask);
 	
 	if (isBaro) {
 		altitude = getAltSmoothed(baro_e_life);
-		store(altitude);
-		if(altitude > altApogee) { altApogee = altitude; }
-	} else { store(0.0); }
+		store(altitude); // store altitude value
+		if(altitude > altApogee) { altApogee = altitude; } // check for apogee while we're here (this should really be reorganized)
+	} else { store(0.0); } // if we don't have a reading, store dummy values to keep spacing
 	if(isIMU) {
-		store(getAccelX());
+		store(getAccelX()); // store accel/gyro values
 		store(getAccelY());
 		store(getAccelZ());
 		store(getTilt());
@@ -306,7 +303,7 @@ void ArdIU::logData(int baro_e_life) {
 		store(0.0);
 		store(0.0);
 		store(0.0);
-	}
+	} // if we don't have a reading, store dummy values to keep spacing
 	
 }
 byte ArdIU::setFlag(long int met, void (*event)()) {
@@ -319,60 +316,61 @@ byte ArdIU::setFlag(long int met, void (*event)()) {
 }
 
 void ArdIU::getFlags() {
-	for(int i = 0; i < N_FLAGS; i++) { flagBuffer[i].getEvent(); }
+	for(int i = 0; i < N_FLAGS; i++) { flagBuffer[i].getEvent(); } // check each flag in the array
 }
 
-void endFiring() {
+void endFiring() { // flag function to shut off all pyros
 	for(int i = 0; i < CHANNELS; i++) {
 		digitalWrite(ArdIU::pyroPins[i], LOW);
 	}
 }
-void ArdIU::fire(int channel, int time) {
+void ArdIU::fire(int channel, int time) { // set a pyro channel high, then set a flag to turn it off after a set time
 	if(channel >= 0 && channel < CHANNELS && getCont(channel) && !channelFired[channel]) {
 		digitalWrite(pyroPins[channel], HIGH);
-		channelFired[channel] = true;
+		channelFired[channel] = true; // record that we fired this channel and shouldn't fire it again
 		setFlag(millis()+time, endFiring);
-	}
+	} // note: this implementation prevents overlapping firings but that shouldn't really affect anything realistically
 }
 
-bool ArdIU::isLiftoff() { return tLiftoff > 0; }
+bool ArdIU::isLiftoff() { return tLiftoff > 0; } // if any time is > 0, we've recorded it
 bool ArdIU::isBurnout() { return tBurnout > 0; }
 bool ArdIU::isApogee() { return tApogee > 0; }
 
-void _atBurnout() { ArdIU::tBurnout = millis(); }
-void _atLiftoff() {
+void _atBurnout() { ArdIU::tBurnout = millis(); } // function to be run when we detect burnout
+void _atLiftoff() { // function to be run when we detect liftoff
 	ArdIU::tLiftoff = millis();
 	ArdIU::vertical = BetterVectorFloat(ArdIU::getAccelX(), ArdIU::getAccelY(), ArdIU::getAccelZ());
-	ArdIU::vertical.normalize();
+	ArdIU::vertical.normalize(); // when liftoff detected, record our vertical vector to be our current acceleration
 }
-void _atApogee() { ArdIU::tApogee = millis(); }
+void _atApogee() { ArdIU::tApogee = millis(); } // function to be run when we detect apogee
 
-void ArdIU::restartFlag(byte &flag, void (*event)(), int time)
-{
-	if(flag != NO_FLAG) { flagBuffer[flag].setNotLive(); }
-	flag = setFlag(millis()+time, event);
+void ArdIU::restartFlag(byte &flag, void (*event)(), int time) {
+	if(flag != NO_FLAG) { flagBuffer[flag].setNotLive(); } // if the flag exists, remove it
+	flag = setFlag(millis()+time, event); // and put a new one there with a reset timer and store the new location in flag
 }
 
+// Flight event detection is done via watchdog flags that are constantly reset whenever conditions are outside event parameters;
+// only if conditions stay favorable long enough for the timer to burn completely does the event fire
 void ArdIU::getLiftoff(float threshhold, int time) {
-	if(getAccel() < threshhold && !isLiftoff()) {
+	if(getAccel() < threshhold && !isLiftoff()) { // if we detect insufficient acceleration, reset the flag's timer
 		restartFlag(liftoffFlag, _atLiftoff, time);
 	}
 }
 void ArdIU::getBurnout(int time) {
 	BetterVectorFloat accf = BetterVectorFloat(accel.x, accel.y, accel.z);
-	if(accf.dotProduct(vertical) > 0 && !isBurnout()) {
+	if(accf.dotProduct(vertical) > 0 && !isBurnout()) { // if we're accelerating upward, reset the flag's timer
 		restartFlag(burnoutFlag, _atBurnout, time);
 	}
 }
 
 void ArdIU::getApogee(int time, int altDrop) {
-	if(altitude > altApogee - altDrop && !isApogee()) {
-        restartFlag(apogeeFlag, _atApogee, time);
+	if(altitude > altApogee - altDrop && !isApogee()) { // if we aren't more than a certain margin below our apogee, reset the flag's timer
+      		restartFlag(apogeeFlag, _atApogee, time);
 	}
 }
 
 float ArdIU::getTilt() {
-	BetterVectorFloat v = BetterVectorFloat(vertical.x, vertical.y, vertical.z);
-	v.rotate(&imuQ);
-	return acos(v.dotProduct(vertical));
+	BetterVectorFloat v = BetterVectorFloat(vertical.x, vertical.y, vertical.z); copy the vertical vector
+	v.rotate(&imuQ); // rotate the vertical vector by the measured rotation
+	return acos(v.dotProduct(vertical)); // find the angle between it and the original vertical
 }
